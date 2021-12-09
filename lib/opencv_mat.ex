b9840a6ccd8a398e0099ad105221221197f2c315
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-09 17:43:05 +0900

    implement to_nx temporary

--- lib/opencv_mat.ex
+++ lib/opencv_mat.ex
@@ -16,4 +16,20 @@ defmodule OpenCV.Mat do
   def to_binary(mat) do
     :erl_cv_nif.evision_cv_mat_to_binary([img: mat])
   end
+
+  def to_nx(mat) do
+    unless is_nil(mat) do
+      case {type(mat), shape(mat), to_binary(mat), } do
+        {{:ok, type}, {:ok, shape}, {:ok, binary}} ->
+          {
+            :ok,
+            Nx.from_binary(binary, type)
+            |> Nx.reshape(shape)
+          }
+        _ -> {:error, "Unknown Mat type"}
+      end
+    else
+      {:ok, nil}
+    end
+  end
 end
